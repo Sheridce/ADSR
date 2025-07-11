2f5eb5c43971f1038b4dedba89530f18f3c877e4
Chris Sheridan <csheri46@gmail.com>
2025-07-11 15:27:52 -0400

    modified:   server/index.js 	modified:   server/package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
+//index.js
+
 const express = require('express');
 const app = express();
-const port = 8080;
+const cors = require('cors');
 
-app.get('/', (req, res) =>{
-    res.send('Hello world!');
+app.use(cors())
+
+app.get('/', (req, res) => {
+      res.send('Hello from our server!')
 })
 
-app.listen(port, () => {
-    console.log('Server listening on port ${port}');
+app.listen(8080, () => {
+      console.log('server listening on port 8080')
 })
