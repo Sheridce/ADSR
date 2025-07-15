2651d45e37c7f12eb1ec690f0de71014cc20c748
Chris Sheridan <csheri46@gmail.com>
2025-07-14 21:57:18 -0400

    modified:   .gitignore 	modified:   server/package.json 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,16 @@ const cors = require('cors');
 const corsOptions = {
     origin: ["http://localhost:5173"]
 };
+require('@dotenvx/dotenvx').config()
+const Pool = require('pg').Pool;
+
+const pool = new Pool ({
+    user: process.env.DB_USER,
+    host: 'localhost',
+    database: 'ads-db',
+    password: process.env.DB_PASSWORD,
+    port: 5432
+})
 
 app.use(cors(corsOptions));
 
