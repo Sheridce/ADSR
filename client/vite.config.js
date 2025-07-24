54389237b035f865963c9a1923cf288d7851bfea
Chris Sheridan <csheri46@gmail.com>
2025-07-24 14:34:43 -0400

    modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        survey: resolve(__dirname, 'survey.html'),
+      },
+    },
+  },
+});
